c6a3d229d0af38218a0bef98d94227dd5a210455
syoterun <syoterun@gmail.com>
2022-08-17 06:40:32 +0900

    Class update

--- main.py
+++ main.py
@@ -6,77 +6,99 @@ import tweepy
 from time import sleep
 import urllib.error
 import urllib.request
-import tkinter as Tkinter
+import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import configparser
-
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-
-root = Tkinter.Tk()
-root.title(u"動画一括保存")
-root.geometry("350x50")
-root.resizable(False, False)
 
 config_ini = configparser.ConfigParser()
 config_ini.read('config.ini', encoding='utf-8')
 CONSUMER_KEY = config_ini['CONSUMER']['KEY']
 CONSUMER_SECRET = config_ini['CONSUMER']['SECRET']
-
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 api = tweepy.API(auth)
 
-#動画保存
-def click(event):
-  root.after(1, set_target)
-def set_target():
-  key_account = txt.get()
-  count_no = int(txt2.get())
-  search_results = tweepy.Cursor(api.user_timeline, screen_name=key_account).items(count_no)
-  if not os.path.exists('img'):
-    os.mkdir('img')
-  i = 0
-  for result in search_results:
-    try:
-      #動画
-      video_url = [variant['url'] for variant in result.extended_entities["media"][0]["video_info"]['variants'] if variant['content_type'] == 'video/mp4'][0]
-      print(video_url)
-      dst_path = 'img/{}.mp4'.format(i)
-      download_file(video_url,dst_path)
-      i += 1
-      sleep(1)
-    except:
-      pass
-  messagebox.showinfo('成功', '保存完了しました')
+class Application(tk.Frame):
+    def __init__(self,master):
+        super().__init__(master)
+        self.pack()
 
-def download_file(url, dst_path):
-   try:
-       with urllib.request.urlopen(url) as web_file:
-           data = web_file.read()
-           with open(dst_path, mode='wb') as local_file:
-               local_file.write(data)
-   except urllib.error.URLError as e:
-       print(e)
+        self.master.geometry("250x130")
+        self.master.title(u"動画一括保存")
 
-# ラベル
-lbl = Tkinter.Label(text='対象アカウント')
-lbl.place(x=10,y=3)
-# 対象アカウント
-txt = Tkinter.Entry(width=20)
-txt.place(x=10,y=20)
-
-# ラベル
-lbl = Tkinter.Label(text='対象ツイート数')
-lbl.place(x=150,y=3)
-# 対象ツイート数
-txt2 = Tkinter.Entry(width=20)
-txt2.place(x=150,y=20)
-
-#実行ボタン
-button2 = Tkinter.Button(root, text=u'実行',width=5)
-button2.bind("<Button-1>",click)
-button2.place(x=300,y=15)
+        self.create_widgets()
 
 
-root.mainloop()
+    # Create Widgets function
+    def create_widgets(self):
+        #Label
+        self.label_target = ttk.Label(self)
+        self.label_target.configure(text='対象アカウント')
+        self.label_target.pack()
+
+        #Entry
+        self.target = tk.StringVar()
+        self.entry_target = ttk.Entry(self)
+        self.entry_target.configure(textvariable = self.target)
+        self.entry_target.pack()
+
+        #Label2
+        self.label_count=ttk.Label(self)
+        self.label_count.configure(text = '対象ツイート数')
+        self.label_count.pack()
+
+        #Entry2
+        self.count = tk.StringVar()
+        self.entry_count = ttk.Entry(self)
+        self.entry_count.configure(textvariable = self.count)
+        self.entry_count.pack()
+
+        #Button
+        self.button_run = ttk.Button(self)
+        self.button_run.configure(text="実行")
+        self.button_run.configure(command = self.click) #do not forget to add self!
+        self.button_run.pack()
+
+    # Event Callback Function
+    def say_Hello(self):
+        print("Hello, World")  # on python console
+        self.label_hello.configure(text="I Have benn Clicked!")
+        print(self.name.get())
+        self.label_name.configure(text=self.name.get())
+
+    def click(self):
+        self.after(1, self.download_movies)
+
+    def download_movies(self): 
+        search_results = tweepy.Cursor(api.user_timeline, screen_name = self.target.get()).items(int(self.count.get()))
+        if not os.path.exists('img'):
+            os.mkdir('img')
+        i = 0
+        for result in search_results:
+            try:
+                #動画
+                movie_url = [variant['url'] for variant in result.extended_entities["media"][0]["video_info"]['variants'] if variant['content_type'] == 'video/mp4'][0]
+                print(movie_url)
+                dst_path = 'img/{}.mp4'.format(i)
+                try:
+                    with urllib.request.urlopen(movie_url) as web_file:
+                        data = web_file.read()
+                        with open(dst_path, mode='wb') as local_file:
+                            local_file.write(data)
+                except urllib.error.URLError as e:
+                    print(e)
+                i += 1
+                sleep(1)
+            except:
+                pass
+        messagebox.showinfo('成功', '保存完了しました')
+        print('exit')
+
+def main():
+    root = tk.Tk()
+    app = Application(master=root)#Inheritクラスの継承！
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
